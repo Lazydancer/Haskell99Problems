b65d37534b6cf3e811a84da840a72ad46add4d3e
Lazydancer <jrpucula@gmail.com>
2013-08-07 21:07:40 -0400

    fourteen, fifteen, sixteen

---
+++ 14.hs
@@ -0,0 +1,19 @@
+{-
+4 Problem 14
+
+(*) Duplicate the elements of a list.
+
+Example:
+
+* (dupli '(a b c c d))
+(A A B B C C C C D D)
+
+Example in Haskell:
+
+> dupli [1, 2, 3]
+[1,1,2,2,3,3]
+-}
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:(dupli xs) 
---
+++ 16.hs
@@ -0,0 +1,18 @@
+{-
+ 6 Problem 16
+
+(**) Drop every N'th element from a list.
+
+Example:
+
+* (drop '(a b c d e f g h i k) 3)
+(A B D E G H K)
+
+Example in Haskell:
+
+*Main> dropEvery "abcdefghik" 3
+"abdeghk"
+-}
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery = 
